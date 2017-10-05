724ce3df3cba8d94a748558db1cf1b80ab4ef17b
Marton <mpochavillo@gmail.com>
2017-10-05 09:27:37 +0800

    Completed header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,3 @@
-console.log('this is a test');
+import MobileMenu from './modules/MobileMenu';
+
+var mobileMenu = new MobileMenu();
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+//this is webpack and this is to create a file that browser will read. It will copy whats in the scripts/App.js and 
+// modify it in temp/scripts/App.js
+
+
+var path = require('path'); //part of node to make path an absolute path
+
+
+module.exports = {
+	entry: './app/assets/scripts/App.js',     //this is to tell webpack w/c file it should begin looking at to create its bundle (App.js)
+	output: {									//where the final product should be bundled to
+	  path: path.resolve(__dirname, './app/temp/scripts'), //must be absolute path
+	  filename: 'App-bundled.js'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}									  
+}	
+
+//after running webpack to commandline change the path in index.html to the consolidated js bundle in temp foLder
